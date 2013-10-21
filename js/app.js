cb10cddf4dda923a5dbc45dddcda1b5967973b3f
Metin Ucar <metin.ucar@standout.se>
2013-10-21 15:41:04 +0200

    - Modifies navigation behavior - Optimizes existing functions - Adds a new jQuery library, Enquire, for responsive queries

--- js/app.js
+++ js/app.js
@@ -1,83 +1,69 @@
 $(document).ready(function() {
 
-// Secondary Navigation Show/Hide & Controlling the Content Width
-	$('.default-actions .primary span').on('click', function(){
-		$('.nav-secondary').toggleClass('minimized');
-		$('.app .content').toggleClass('full-width');
-		$(this).parent().next().toggleClass('inactive');
-	});
+// PANEL ACTIONS
+	$('.panel-heading .btn-group').on('click', '.btn', function(){
+		
+		var panelWrap = $(this).parents('.panel');
+		$(this).toggleClass('active').siblings('.btn.active').removeClass('active');
 
-	$('.nav-primary a').on('click', function(){
-		$('.nav-secondary.minimized').toggleClass('minimized');
-		$('.app .content.full-width').toggleClass('full-width');
-	});
-
-// Panel Toggle
-	$('.panel-toggle').on('click', function(){
-		$(this).toggleClass('active').parents('.panel').toggleClass('minimized').find('.panel-body').slideToggle(700);
-		if($(this).siblings('.btn').prop('disabled') === true) {
-			$(this).siblings('.btn').prop('disabled',false);
-		} else {
-			$(this).siblings('.btn').prop('disabled',true);
+		// Panel Toggle
+		if($(this).hasClass('panel-toggle')) {
+			panelWrap.toggleClass('minimized').find('.panel-body').slideToggle(400);
 		}
+		
+		// Panel Toggle Settings
+		if($(this).hasClass('panel-toggle-settings')) {
+			if(panelWrap.hasClass('minimized') === true) {
+				panelWrap.toggleClass('minimized').find('.panel-body').slideToggle(400);
+				panelWrap.find('.panel-settings').slideToggle(400);
+			} else {
+				panelWrap.find('.panel-settings').slideToggle(600);
+			}
+		}
+		
+		// Panel Toggle Add
+		if($(this).hasClass('panel-toggle-add')) {
+			if(panelWrap.hasClass('minimized') === true) {
+				panelWrap.toggleClass('minimized').find('.panel-body').slideToggle(400);
+				panelWrap.find('.panel-add').slideToggle(400);
+			} else {
+				panelWrap.find('.panel-add').slideToggle(600);
+			}
+		}
+
 	});
 
-// Panel Settings
-	$('.panel-toggle-settings').on('click',function(){
-		$(this).toggleClass('active').parents('.panel').not('.minimized').find('.panel-settings').slideToggle(400);
-	});
-	$('.panel-toggle-add').on('click',function(){
-		$(this).toggleClass('active').parents('.panel').not('.minimized').find('.panel-add').slideToggle(400);
-	});
-	$('.panel-hidden .close').on('click',function(){
+	// Close the panel
+	$('.panel-hidden').on('click', '.close', function(){
 		$(this).parent().slideToggle(400);
 		$(this).parents('.panel').find('.panel-heading .btn.active').toggleClass('active');
 	});
 
 // PRIMARY NAVIGATION
-	// on page load
-		var activePrimaryNavTitle = $('.nav-primary li.active').data('section');
-		$('.nav-secondary ul.hidden[data-section-parent="'+activePrimaryNavTitle+'"]').toggleClass('hidden');
-		$('.default-actions .secondary h2').text(activePrimaryNavTitle);
+	$('.nav-primary a').on('click', function(event){
+		event.preventDefault();
+		$(this).parent('li').addClass('active').siblings('.active').removeClass('active');
 
-		// when clicked
-		$('.nav-primary a').on('click', function(event){
-			event.preventDefault();
-			$(this).parent('li').siblings('.active').removeClass('active');
-			$(this).parent('li').addClass('active');
+		var sectionTitle = $(this).parents('li').data('section'),
+			linkedSection = $('.nav-secondary ul[data-section-parent="'+sectionTitle+'"]');
 
-			var sectionTitle = $(this).parents('li').data('section');
-			$('.nav-secondary ul').not('.hidden').toggleClass('hidden');
-			$('.nav-secondary ul.hidden[data-section-parent="'+sectionTitle+'"]').toggleClass('hidden');
-			$('.default-actions .secondary h2').text(sectionTitle);
-		});
+		if(linkedSection.length > 0) {
 
-	// Make it responsive
-		if (Modernizr.mq('only screen and (max-width: 991px)')) {
-			// Re-organize primary and secondary navigation elements for responsive navigation
-			$('#nav-responsive').appendTo('body').removeClass('nav-primary');
+			$('.nav-secondary h2').text(sectionTitle);
+			$('.nav-secondary.minimized').toggleClass('minimized');
+			$('.app .content.full-width, header.full-width, .section-header.full-width').removeClass('full-width');
 
-			// Move default actions into the wrapper
-			$('.default-actions').prependTo('#wrapper');
-			$('.default-actions .secondary').toggleClass('inactive');
-
-			$('.nav-secondary ul').each(function(){
-				$(this).removeClass('hidden');
-				var subNavSectionTitle = $(this).data('section-parent');
-				$(this).appendTo($('#nav-responsive ul li[data-section="'+subNavSectionTitle+'"]'));
-			});
-
-			$('#nav-responsive').mmenu({
-				configuration: {
-				    hardwareAcceleration : true,
-				    preventTabbing       : true,
-				    transitionDuration   : 500,
-				    pageNodetype: "section"
-				}
-			}); // Initiate plugin
-
-			$('#nav-responsive .mm-subopen').each(function(){ $('<i class="icon-right-open-big"></i>').appendTo($(this));});
-			$('#nav-responsive .mm-subclose').each(function(){ $('<i class="icon-left-open-big"></i>').appendTo($(this));});
+			linkedSection.removeClass('hidden').siblings('ul').addClass('hidden');
+		} else {
+			$('.nav-secondary').addClass('minimized', 800);
+			$('.app .content, header, .section-header').addClass('full-width', 800);
 		}
+		
+	});
 
+	// Secondary Navigation Show/Hide & Controlling the Content Width
+		$('.nav-minimize').on('click', function(){
+			$('.nav-secondary').toggleClass('minimized');
+			$('.app .content, .section-header, header').toggleClass('full-width');
+		});
 });
